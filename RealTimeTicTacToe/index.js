99ac01c6bc1da26ab75cc9830a49b1dc1590680d
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2025-01-07 13:31:06 +0530

    added middleware

--- RealTimeTicTacToe/index.js
+++ RealTimeTicTacToe/index.js
@@ -5,4 +5,11 @@ const path = require('path');
 const http = require('http');
 const {Server} = require('socket.io');
 
+const server =  http.createServer(app);
 
+const io = new Server(server);
+
+// middleware
+app.use(express.static(path.resolve("")))
+
+app.get
