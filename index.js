09b4be1b8ee58e99f2f6fcbc0b19ea98c3e7c1ce
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2025-01-28 15:06:29 +0530

    folder changes

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get("/", (req, res) =>{
     return res.sendFile("index.html");
 })
 
-server.listen(3000, ()=>{
-    console.log("Port connected to 3000");
+server.listen(3000, () =>{
+    console.log("port connected to 3000");
 })
 
